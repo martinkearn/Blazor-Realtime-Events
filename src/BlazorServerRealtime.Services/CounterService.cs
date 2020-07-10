d2ecfcc1f956dd403a0caeb5d61e387ea8f71438
Martin Kearn <martin.kearn@microsoft.com>
2020-07-10 11:26:19 +0100

    Initial project (#1)
    * Initial project
    
    * Add service project and initial service
    
    * Initial pages
    
    * Final tweaks

---
+++ src/BlazorServerRealtime.Services/CounterService.cs
@@ -0,0 +1,35 @@
+﻿using BlazorServerRealtime.Services.Interfaces;
+using System;
+
+namespace BlazorServerRealtime.Services
+{
+    /// <summary>
+    /// Concrete implementation of ICounterService.
+    /// </summary>
+    public class CounterService : ICounterService
+    {
+        private int counter;
+
+        public CounterService()
+        {
+            counter = 0;
+        }
+
+        /// <inheritdoc />
+        public int Get()
+        {
+            return counter;
+        }
+
+        /// <inheritdoc />
+        public int Add(int numberToAdd) 
+        {
+            counter += numberToAdd;
+            CounterChanged?.Invoke(this, counter);
+            return counter;
+        }
+
+        /// <inheritdoc />
+        public event EventHandler<int> CounterChanged;
+    }
+}
---
+++ src/BlazorServerRealtime.Services/Interfaces/ICounterService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BlazorServerRealtime.Services.Interfaces
+{
+    public interface ICounterService
+    {
+        /// <summary>
+        /// Gets the current value of the counter.
+        /// </summary>
+        /// <returns>The current value of the counter.</returns>
+        int Get();
+
+        /// <summary>
+        /// Adds a number to the counter.
+        /// </summary>
+        /// <param name="numberToAdd">The number to add to the counter.</param>
+        /// <returns>The new counter total.</returns>
+        int Add(int numberToAdd);
+
+        /// <summary>
+        /// An event handler that fires whenever the counter changes.
+        /// </summary>
+        event EventHandler<int> CounterChanged;
+    }
+}
