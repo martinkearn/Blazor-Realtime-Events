7a63679770bc387a7faca667dc3b428cc23beb34
Martin Kearn <martin.kearn@microsoft.com>
2020-07-10 10:44:33 +0100

    Add service project and initial service

---
+++ src/BlazorServerRealtime.Services/CounterService.cs
@@ -0,0 +1,29 @@
+﻿using BlazorServerRealtime.Services.Interfaces;
+using System;
+
+namespace BlazorServerRealtime.Services
+{
+    /// <summary>
+    /// Concrete implementation of ICounterService.
+    /// </summary>
+    public class CounterService : ICounterService
+    {
+        private int counter;
+
+        public CounterService()
+        {
+            counter = 0;
+        }
+
+        /// <inheritdoc />
+        public int Add(int numberToAdd) 
+        {
+            counter += numberToAdd;
+            CounterChanged?.Invoke(this, counter);
+            return counter;
+        }
+
+        /// <inheritdoc />
+        public event EventHandler<int> CounterChanged;
+    }
+}
---
+++ src/BlazorServerRealtime.Services/Interfaces/ICounterService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BlazorServerRealtime.Services.Interfaces
+{
+    public interface ICounterService
+    {
+        /// <summary>
+        /// Adds a number to the counter.
+        /// </summary>
+        /// <param name="numberToAdd">The number to add to the counter.</param>
+        /// <returns>The new counter total.</returns>
+        int Add(int numberToAdd);
+
+        /// <summary>
+        /// An event handler that fires whenever the counter changes.
+        /// </summary>
+        event EventHandler<int> CounterChanged;
+    }
+}
